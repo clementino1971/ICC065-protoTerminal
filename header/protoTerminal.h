72f7beb74860ee1736af9d899321a60915734cc2
foolnando <fernandoonv06@gmail.com>
2020-06-09 14:12:05 -0400

    inclusao de algumas libs que eu tinha esquecido de adicionar

--- header/protoTerminal.h
+++ header/protoTerminal.h
@@ -1,9 +1,12 @@
 #ifndef PROTOTERMINAL_H
 #define PROTOTERMINAL_H
 
-#include <dirent.h>
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<dirent.h>
+#include<errno.h>
+
 
 
 void ls(const char *dir); //operação de list, nao print com cores difernetes nem em colunas
