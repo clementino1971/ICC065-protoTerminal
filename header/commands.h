b82b602f2dc20eaddc794d5e4b9d8bda885ea1f1
foolnando <fernandoonv06@gmail.com>
2020-06-10 20:51:15 -0400

    adicionado o comando cd para mudança de diretório.

--- header/commands.h
+++ header/commands.h
@@ -7,8 +7,10 @@
 #include<dirent.h>
 #include<errno.h>
 #include <unistd.h>
+#include <pwd.h>
 
 
 void ls(char *dir); //operação de list, nao print com cores difernetes nem em colunas
 void pwd(char *dir); //da o caminho do diretorio atual 
+void cd(const char *dir, char * currDir); //muda o diretorio
 #endif
